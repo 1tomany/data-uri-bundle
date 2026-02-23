31443d986f67480b0b5cf6798bb963394b39e132
Vic Cherubini <vcherubini@gmail.com>
2026-02-23 12:58:14 -0600

    [#3] Add a console command to encode a file to a data URL (#4)
    resolves #3

---
+++ src/Command/EncodeFileCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace OneToMany\DataUriBundle\Command;
+
+use OneToMany\DataUri\DataDecoder;
+use Symfony\Component\Console\Attribute\Argument;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class EncodeFileCommand extends Command
+{
+    public function __invoke(
+        OutputInterface $output,
+        #[Argument('Path to the file to encode')] string $path,
+    ): int {
+        $output->write(new DataDecoder()->decode($path)->toDataUri(), false, OutputInterface::OUTPUT_RAW);
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * @see Symfony\Component\Console\Command\Command
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setName('data-uri:encode-file')
+            ->setDescription('Outputs a file as a base64 encoded data URL');
+    }
+}
