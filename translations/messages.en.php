38d0bb302b31d0e85706494ac1f89ff4eb7da569
Vic Cherubini <vcherubini@gmail.com>
2026-07-17 11:54:23 -0500

    wip

--- translations/messages.en.php
+++ translations/messages.en.php
@@ -3,6 +3,6 @@
 use OneToMany\DataUri\Contract\Enum\Type;
 
 // Map the file type onto itself to avoid having to manually maintain this list
-$fileTypeNames = \array_map(fn (Type $t): string => $t->getName(), Type::cases());
+$fileTypeNames = \array_map(static fn (Type $t): string => $t->getName(), Type::cases());
 
 return \array_combine($fileTypeNames, $fileTypeNames);
