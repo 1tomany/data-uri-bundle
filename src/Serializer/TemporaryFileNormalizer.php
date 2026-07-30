88829953aaf69345d114f9f57f03c7c3211de238
Vic Cherubini <vcherubini@gmail.com>
2026-07-30 12:56:22 -0500

    [#15] Upgrade `1tomany/data-uri` to v7.0.0 (#16)
    resolves #15

---
+++ src/Serializer/TemporaryFileNormalizer.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace OneToMany\DataUriBundle\Serializer;
+
+use OneToMany\DataUri\Contract\Record\TemporaryFileInterface;
+use OneToMany\DataUri\DataDecoder;
+use OneToMany\DataUri\Exception\InvalidArgumentException;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+use function count;
+use function filter_var;
+use function get_debug_type;
+use function is_a;
+use function is_array;
+use function is_string;
+use function sprintf;
+use function stripos;
+
+use const FILTER_VALIDATE_URL;
+
+final readonly class TemporaryFileNormalizer implements DenormalizerInterface, NormalizerInterface
+{
+    public function __construct(
+        private DataDecoder $dataDecoder = new DataDecoder(),
+    ) {
+    }
+
+    /**
+     * @see Symfony\Component\Serializer\Normalizer\DenormalizerInterface
+     *
+     * @param string|\Stringable|File $data
+     *
+     * @throws InvalidArgumentException when the data is an invalid file {@see Symfony\Component\HttpFoundation\File\UploadedFile}
+     * @throws InvalidArgumentException when the data is an unexpected type
+     */
+    public function denormalize(mixed $data, string $type, ?string $format = null, array $context = []): TemporaryFileInterface
+    {
+        if ($data instanceof File) {
+            if ($data instanceof UploadedFile) {
+                if (!$data->isValid()) {
+                    throw new InvalidArgumentException($data->getErrorMessage());
+                }
+
+                $name = $data->getClientOriginalName();
+            }
+
+            return $this->dataDecoder->decode($data, name: $name ?? $data->getFilename());
+        }
+
+        if ($data instanceof \Stringable) {
+            $data = $data->__toString();
+        }
+
+        if (!is_string($data)) { // @phpstan-ignore function.alreadyNarrowedType
+            throw new InvalidArgumentException(sprintf('Expected data of type "%s", "%s" given.', 'string', get_debug_type($data)));
+        }
+
+        // The data is not a URL or an encoded URI,
+        // so we assume it is a block of plaintext
+        if (true === $this->isPlaintextData($data)) {
+            return $this->dataDecoder->decodeText($data);
+        }
+
+        return $this->dataDecoder->decode($data);
+    }
+
+    /**
+     * @see Symfony\Component\Serializer\Normalizer\NormalizerInterface
+     *
+     * @param TemporaryFileInterface $data
+     *
+     * @return array{
+     *   name: non-empty-string,
+     *   size: non-negative-int,
+     *   format: non-empty-lowercase-string,
+     * }
+     *
+     * @throws InvalidArgumentException when the data is an unexpected type {@see OneToMany\DataUri\Contract\Record\TemporaryFileInterface}
+     */
+    public function normalize(mixed $data, ?string $format = null, array $context = []): array
+    {
+        if (!$data instanceof TemporaryFileInterface) { // @phpstan-ignore instanceof.alwaysTrue
+            throw new InvalidArgumentException(sprintf('Expected data of type "%s", "%s" given.', TemporaryFileInterface::class, get_debug_type($data)));
+        }
+
+        return [
+            'name' => $data->getName(),
+            'size' => $data->getSize(),
+            'format' => $data->getFormat(),
+        ];
+    }
+
+    /**
+     * @see Symfony\Component\Serializer\Normalizer\DenormalizerInterface
+     * @see https://github.com/1tomany/data-uri-bundle/issues/1
+     *
+     * @param class-string<TemporaryFileInterface> $type
+     */
+    public function supportsDenormalization(mixed $data, string $type, ?string $format = null, array $context = []): bool
+    {
+        $supportsDenormalization = false;
+
+        if (!is_a($type, TemporaryFileInterface::class, true)) { // @phpstan-ignore function.alreadyNarrowedType
+            return $supportsDenormalization;
+        }
+
+        $isDataSupported = static function (mixed $value): bool {
+            return
+                is_string($value)
+                || $value instanceof File
+                || $value instanceof \Stringable
+            ;
+        };
+
+        if ($isDataSupported($data)) {
+            $supportsDenormalization = true;
+        } else {
+            if (is_array($data)) {
+                if ($count = count($data)) {
+                    $supportedCount = 0;
+
+                    foreach ($data as $dv) {
+                        if ($isDataSupported($dv)) {
+                            ++$supportedCount;
+                        }
+                    }
+
+                    $supportsDenormalization = $count === $supportedCount;
+                }
+            }
+        }
+
+        return $supportsDenormalization;
+    }
+
+    /**
+     * @see Symfony\Component\Serializer\Normalizer\NormalizerInterface
+     */
+    public function supportsNormalization(mixed $data, ?string $format = null, array $context = []): bool
+    {
+        return $data instanceof TemporaryFileInterface;
+    }
+
+    /**
+     * @see Symfony\Component\Serializer\Normalizer\DenormalizerInterface
+     * @see Symfony\Component\Serializer\Normalizer\NormalizerInterface
+     */
+    public function getSupportedTypes(?string $format): array
+    {
+        return [
+            TemporaryFileInterface::class => true,
+        ];
+    }
+
+    /**
+     * @see https://github.com/1tomany/rich-bundle/issues/66
+     */
+    private function isPlaintextData(string $data): bool
+    {
+        $isHttpUrl = false !== filter_var($data, FILTER_VALIDATE_URL) && 0 === stripos($data, 'http');
+
+        if ($isHttpUrl) {
+            return false;
+        }
+
+        return false === stripos($data, 'data:');
+    }
+}
---
+++ tests/Serializer/TemporaryFileNormalizerTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace OneToMany\DataUriBundle\Tests\Serializer;
+
+use OneToMany\DataUri\Contract\Record\TemporaryFileInterface;
+use OneToMany\DataUri\Exception\InvalidArgumentException;
+use OneToMany\DataUriBundle\Serializer\TemporaryFileNormalizer;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+use function basename;
+use function get_debug_type;
+
+use const UPLOAD_ERR_PARTIAL;
+
+#[Group('UnitTests')]
+#[Group('SerializerTests')]
+final class TemporaryFileNormalizerTest extends TestCase
+{
+    public function testDenormalizingUploadedFileRequiresItToBeValid(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessageIs('The file "photo.jpeg" was only partially uploaded.');
+
+        new TemporaryFileNormalizer()->denormalize(new UploadedFile('/path/to/photo.jpeg', 'photo.jpeg', 'image/jpeg', UPLOAD_ERR_PARTIAL, true), TemporaryFileInterface::class);
+    }
+
+    public function testDenormalizingFileUsesFilename(): void
+    {
+        $file = new TemporaryFileNormalizer()->denormalize(new File(__FILE__), TemporaryFileInterface::class);
+
+        $this->assertEquals('TemporaryFileNormalizerTest.php', $file->getName());
+    }
+
+    public function testDenormalizingUploadedFileUsesClientOriginalName(): void
+    {
+        $file = new TemporaryFileNormalizer()->denormalize(new UploadedFile(__FILE__, basename(__FILE__), 'text/x-php', test: true), TemporaryFileInterface::class);
+
+        $this->assertEquals('TemporaryFileNormalizerTest.php', $file->getName());
+    }
+
+    public function testDenormalizingNonSymfonyFileRequiresStringOrStringableData(): void
+    {
+        $data = new \DateTimeImmutable();
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessageIs('Expected data of type "string", "'.get_debug_type($data).'" given.');
+
+        new TemporaryFileNormalizer()->denormalize($data, TemporaryFileInterface::class); // @phpstan-ignore argument.type
+    }
+
+    public function testDenormalizingStringableNonSymfonyFileObject(): void
+    {
+        $data = new class implements \Stringable {
+            public function __toString(): string
+            {
+                return 'data:text/plain;base64,SGVsbG8sIHdvcmxkIQ==';
+            }
+        };
+
+        $file = new TemporaryFileNormalizer()->denormalize($data, TemporaryFileInterface::class);
+
+        $this->assertTrue($file->getType()->isTxt());
+        $this->assertEquals('Hello, world!', $file->read());
+    }
+
+    public function testDenormalizingText(): void
+    {
+        $file = new TemporaryFileNormalizer()->denormalize('Hello, world!', TemporaryFileInterface::class);
+
+        $this->assertTrue($file->getType()->isTxt());
+        $this->assertEquals('Hello, world!', $file->read());
+    }
+
+    public function testDenormalizingDataUri(): void
+    {
+        $file = new TemporaryFileNormalizer()->denormalize('data:text/plain;base64,SGVsbG8sIHdvcmxkIQ==', TemporaryFileInterface::class);
+
+        $this->assertTrue($file->getType()->isTxt());
+        $this->assertEquals('Hello, world!', $file->read());
+    }
+
+    public function testDoesNotSupportNormalizationWithNonStringAndNonSymfonyFileDataAndTemporaryFileInterface(): void
+    {
+        $this->assertFalse(new TemporaryFileNormalizer()->supportsDenormalization(new \stdClass(), TemporaryFileInterface::class));
+    }
+
+    public function testSupportsNormalizationWithStringDataAndTemporaryFileInterfaceType(): void
+    {
+        $this->assertTrue(new TemporaryFileNormalizer()->supportsDenormalization('Hello, world!', TemporaryFileInterface::class));
+    }
+
+    public function testSupportsNormalizationWithSymfonyFileDataAndTemporaryFileInterfaceType(): void
+    {
+        $this->assertTrue(new TemporaryFileNormalizer()->supportsDenormalization(new File('file.pdf', false), TemporaryFileInterface::class));
+    }
+
+    public function testDoesNotSupportNormalizationWithEmptyListDataAndTemporaryFileInterfaceType(): void
+    {
+        $this->assertFalse(new TemporaryFileNormalizer()->supportsDenormalization([], TemporaryFileInterface::class));
+    }
+
+    public function testDoesNotSupportNormalizationWithNonEmptyListOfNonStringAndNonSymfonyFileDataAndTemporaryFileInterfaceType(): void
+    {
+        $this->assertFalse(new TemporaryFileNormalizer()->supportsDenormalization(['Hello, world!', null, new \stdClass()], TemporaryFileInterface::class));
+    }
+
+    public function testSupportsNormalizationWithNonEmptyListOfStringsDataAndTemporaryFileInterfaceType(): void
+    {
+        $this->assertTrue(new TemporaryFileNormalizer()->supportsDenormalization(['Hello, world!'], TemporaryFileInterface::class));
+    }
+
+    public function testSupportsNormalizationWithNonEmptyListOfSymfonyFileDataAndTemporaryFileInterfaceType(): void
+    {
+        $this->assertTrue(new TemporaryFileNormalizer()->supportsDenormalization([new File('file.pdf', false)], TemporaryFileInterface::class));
+    }
+
+    public function testSupportsNormalizationWithNonEmptyListOfStringAndSymfonyFileDataAndTemporaryFileInterfaceType(): void
+    {
+        $data = [
+            'Hello, world!',
+            new File('file.pdf', false),
+            'data:text/plain;base64,SGVsbG8sIHdvcmxkIQ==',
+        ];
+
+        $this->assertTrue(new TemporaryFileNormalizer()->supportsDenormalization($data, TemporaryFileInterface::class));
+    }
+}
